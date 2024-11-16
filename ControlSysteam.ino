cf0c5cdc314d80d40c0f2eb39d53d87693b1155e
AlessanderMorales <alessmoralesmo@gmail.com>
2024-11-16 01:54:17 -0400

    segundo commit

--- ControlSysteam.ino
+++ ControlSysteam.ino
@@ -1,13 +1,12 @@
-#include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "SensorLDR.h"
 #include "RelayControl.h"
 #include "WiFiManager.h"
+
 #define RELAY_PIN 4
 #define LDR_PIN 5
-
 
 const char* WIFI_SSID = "HUAWEI nova 8i";
 const char* WIFI_PASS = "LoreMKC072";
@@ -16,112 +15,102 @@ const int MQTT_PORT = 8883;
 const char* CLIENT_ID = "Relay";
 const char* UPDATE_TOPIC = "$aws/things/Relay/shadow/update";
 const char* UPDATE_DELTA_TOPIC = "$aws/things/Relay/shadow/update/delta";
+
 WiFiClientSecure wiFiClient;
 PubSubClient client(wiFiClient);
+WiFiManager wifiManager(WIFI_SSID, WIFI_PASS);
+SensorLDR ldr(LDR_PIN);
+RelayControl relay(RELAY_PIN);
+
+byte LDRSensor = 0;
 StaticJsonDocument<JSON_OBJECT_SIZE(64)> inputDoc;
 StaticJsonDocument<JSON_OBJECT_SIZE(4)> outputDoc;
 char outputBuffer[128];
-SensorLDR ldr(LDR_PIN);
-RelayControl relay(RELAY_PIN);
-byte builtInLed = 0;
 
-void reportBuiltInLed() {
-  outputDoc["state"]["reported"]["builtInLed"] = builtInLed;
-  serializeJson(outputDoc, outputBuffer);
-  client.publish(UPDATE_TOPIC, outputBuffer);
+void reportLDRSensor() {
+    outputDoc["state"]["reported"]["LDRSensor"] = LDRSensor;
+    serializeJson(outputDoc, outputBuffer);
+    client.publish(UPDATE_TOPIC, outputBuffer);
 }
 
-void setBuiltInLed() {
-  if (builtInLed == 1) {
-    if (ldr.isDark()) {
-      relay.turnOn();
-      Serial.println("Oscuridad detectada. Relé encendido.");
+void setLDRSensor() {
+    if (LDRSensor == 1) {
+        if (ldr.isDark()) {
+            relay.turnOn();
+            Serial.println("Oscuridad detectada. Rele encendido.");
+        } else {
+            relay.turnOff();
+            Serial.println("Hay luz. Rele apagado.");
+        }
     } else {
-      relay.turnOff();
-      Serial.println("Hay luz. Relé apagado.");
+        relay.turnOff();
+        Serial.println("LDRSensor esta apagado. Rele apagado.");
     }
-  } else {
-    relay.turnOff();
-    Serial.println("builtInLed está apagado. Relé apagado.");
-  }
-  reportBuiltInLed();
-  delay(2000);
+    reportLDRSensor();
+    delay(2000);
 }
-
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  String message;
-  for (int i = 0; i < length; i++) message += String((char) payload[i]);
-  Serial.println("Mensaje desde el tópico " + String(topic) + ": " + message);
-  
-  DeserializationError err = deserializeJson(inputDoc, payload);
-  if (!err) {
-    if (String(topic) == UPDATE_DELTA_TOPIC) {
-      builtInLed = inputDoc["state"]["builtInLed"].as<int8_t>();
-      Serial.print("Nuevo valor de builtInLed: ");
-      Serial.println(builtInLed);
-      setBuiltInLed();
+    String message;
+    for (int i = 0; i < length; i++) message += String((char) payload[i]);
+    Serial.println("Mensaje desde el tópico " + String(topic) + ": " + message);
+
+    DeserializationError err = deserializeJson(inputDoc, payload);
+    if (!err) {
+        if (String(topic) == UPDATE_DELTA_TOPIC) {
+            LDRSensor = inputDoc["state"]["LDRSensor"].as<int8_t>();
+            Serial.print("Nuevo valor de LDRSensor: ");
+            Serial.println(LDRSensor);
+            setLDRSensor();
+        }
     }
-  }
 }
 
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Intentando conexión MQTT...");
+        if (client.connect(CLIENT_ID)) {
+            Serial.println("Conectado");
+            client.subscribe(UPDATE_DELTA_TOPIC);
+            Serial.println("Suscrito a " + String(UPDATE_DELTA_TOPIC));
+            delay(100);
+            reportLDRSensor();
+        } else {
+            Serial.print("Fallido, rc=");
+            Serial.print(client.state());
+            Serial.println(" intentando de nuevo en 5 segundos");
+            delay(5000);
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
-  ldr.begin();
-  relay.begin();
-  setupWiFi();
-  wiFiClient.setCACert(AMAZON_ROOT_CA1);
-  wiFiClient.setCertificate(CERTIFICATE);
-  wiFiClient.setPrivateKey(PRIVATE_KEY);
-  client.setServer(MQTT_BROKER, MQTT_PORT);
-  client.setCallback(callback);
+    Serial.begin(115200);
+    delay(100);
+    ldr.begin();
+    relay.begin();
+    wifiManager.connect();
+    wiFiClient.setCACert(AMAZON_ROOT_CA1);
+    wiFiClient.setCertificate(CERTIFICATE);
+    wiFiClient.setPrivateKey(PRIVATE_KEY);
+
+    client.setServer(MQTT_BROKER, MQTT_PORT);
+    client.setCallback(callback);
 }
-
-void setupWiFi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Conectando a ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println();
-  Serial.print("Conectado a WiFi. Dirección IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-void reconnect() {  
-  while (!client.connected()) {
-    Serial.print("Intentando conexión MQTT...");
-    if (client.connect(CLIENT_ID)) {
-      Serial.println("Conectado");
-      client.subscribe(UPDATE_DELTA_TOPIC);
-      Serial.println("Suscrito a " + String(UPDATE_DELTA_TOPIC));
-      delay(100);
-      reportBuiltInLed();
-    } else {
-      Serial.print("Fallido, rc=");
-      Serial.print(client.state());
-      Serial.println(" intentando de nuevo en 5 segundos");
-      delay(5000);
-    }
-  }
-}
-
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  if (builtInLed == 1) {
-    setBuiltInLed();
-    delay(1000);
-  }
+    if (!wifiManager.isConnected()) {
+        wifiManager.connect();
+    }
+
+    if (!client.connected()) {
+        reconnect();
+    }
+
+    client.loop();
+
+    if (LDRSensor == 1) {
+        setLDRSensor();
+        delay(1000);
+    }
 }
--- WiFiManager.h
+++ WiFiManager.h
@@ -1,16 +1,35 @@
-#ifndef WIFI_MANAGER_H
-#define WIFI_MANAGER_H
+#ifndef WIFIMANAGER_H
+#define WIFIMANAGER_H
 
 #include <WiFi.h>
 
 class WiFiManager {
 private:
-  const char* ssid;
-  const char* password;
+    const char* ssid;
+    const char* password;
 
 public:
-  WiFiManager(const char* ssid, const char* password);
-  void connect();
+    WiFiManager(const char* ssid, const char* password) : ssid(ssid), password(password) {}
+
+    void connect() {
+        Serial.println();
+        Serial.print("Conectando a WiFi: ");
+        Serial.println(ssid);
+
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+
+        Serial.println("\nConectado a WiFi.");
+        Serial.print("Dirección IP: ");
+        Serial.println(WiFi.localIP());
+    }
+
+    bool isConnected() {
+        return WiFi.status() == WL_CONNECTED;
+    }
 };
 
 #endif
